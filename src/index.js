fefa3496722871782992eed1a22374380ae67585
YaroslavLazorenko <lazorenko85@gmail.com>
2021-10-08 04:36:49 +0300

    Complete task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import './sass/main.scss';
+import './styles/styles.css';
+import colors from './js/colors-array';
+import refs from './js/refs';
+
+const CHANGE_DELAY = 1000;
+const { body, startButton, stopButton } = refs;
+let timerId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeBodyColor = () => {
+  const color = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  body.style.backgroundColor = color;
+};
+
+const onStartButtonClick = () => {
+  startButton.setAttribute('disabled', '');
+  timerId = setInterval(changeBodyColor, CHANGE_DELAY);
+};
+
+const onStopButtonClick = () => {
+  startButton.removeAttribute('disabled');
+  clearInterval(timerId);
+};
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  body: document.querySelector('body'),
+  startButton: document.querySelector('button[data-action="start"]'),
+  stopButton: document.querySelector('button[data-action="stop"]'),
+};
